ba1977d980df9e3eabbdb7c6dd7c622f785c0b8d
thiranjaya <kanujathiranjaya@gmail.com>
2024-01-11 17:24:17 +0530

    change 1

--- calculator.c
+++ calculator.c
@@ -3,6 +3,11 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("welcome to calculator.\nINDEX\n+  Addition\n-  Subtraction\n*  Multiplication\n/  Division");
+    char op;
+    float ans, n1, n2;
+    printf("welcome to calculator.\nINDEX\n+  Addition\n-  Subtraction\n*  Multiplication\n/  Division\n");
+    printf("ENTER YOUR OPERATOR");
+    scanf("%c", &op);
+
     return 0;
 }
