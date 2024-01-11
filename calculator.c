9eec00091c73d0cae9e35ba5f00496c7f4e69c8f
Thiranjaya Siriwardhana <139935683+thirsturn@users.noreply.github.com>
2024-01-11 17:15:03 +0530

    Created calculator.c

---
+++ calculator.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("welcome to calculator.\nINDEX\n+  Addition\n-  Subtraction\n*  Multiplication\n/  Division");
+    return 0;
+}
